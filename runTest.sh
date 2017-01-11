543d99e9c4e82075e9f361ccb97780afa50ceaed
iCub <robotcub-hackers@lists.sourceforge.net>
2017-01-11 19:30:34 +0100

    fixed cmake bugs

--- runTest.sh
+++ runTest.sh
@@ -1,6 +1,6 @@
 CURRENT_DIRECTORY=`dirname $0`
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:CURRENT_DIRECTORY/install
 export YARP_DATA_DIRS=$YARP_DATA_DIRS:CURRENT_DIRECTORY/install
-testrunner -v -s tests/suites/testSuite.xml
-
+export YARP_ROBOT_NAME=icubSim
+#testrunner -v -s tests/suites/testSuite.xml
 
